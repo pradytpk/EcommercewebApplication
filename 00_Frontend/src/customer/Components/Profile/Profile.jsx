72d6631bb156ca264eb9eb944d60797b3848ddb4
Pradeep Kumar <61448614+pradytpk@users.noreply.github.com>
2023-10-20 20:59:41 +0900

    profile code update

--- 00_Frontend/src/customer/Components/Profile/Profile.jsx
+++ 00_Frontend/src/customer/Components/Profile/Profile.jsx
@@ -1,52 +1,51 @@
 //Author: Lakshmi
 import React from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
+import './Profile.css';
 const Profile = () => {
-    const { auth } = useSelector((store) => store);
-    console.log('auth', auth);
+  const { auth } = useSelector((store) => store);
 
-    return (
-    <div className="main profile-container">
-    <h3 className=" profile-title">My Profile</h3>
-            <div className="card">
-                <div className="card-body">
-                    <i className="fa fa-pen fa-xs edit"></i>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <td>First Name</td>
-                                <td>:</td>
-                                <td>{auth?.user?.firstName}</td>
-                            </tr>
-                            <tr>
-                                <td>Last Name</td>
-                                <td>:</td>
-                                <td>{auth?.user?.lastName}</td>
-                            </tr>
+  return (
+    <div className='main profile-container'>
+      <h3 className=' profile-title'>My Profile</h3>
+      <div className='card'>
+        <div className='card-body'>
+          <i className='fa fa-pen fa-xs edit'></i>
+          <table>
+            <tbody>
+              <tr>
+                <td>First Name</td>
+                <td>:</td>
+                <td>{auth?.user?.firstName}</td>
+              </tr>
+              <tr>
+                <td>Last Name</td>
+                <td>:</td>
+                <td>{auth?.user?.lastName}</td>
+              </tr>
 
-                            <tr>
-                                <td>Mobile</td>
-                                <td>:</td>
-                                <td>{auth?.user?.mobile}</td>
-                            </tr>
+              <tr>
+                <td>Mobile</td>
+                <td>:</td>
+                <td>{auth?.user?.mobile}</td>
+              </tr>
 
-                            <tr>
-                                <td>Email</td>
-                                <td>:</td>
-                                <td>{auth?.user?.email}</td>
-                            </tr>
-                            <tr>
-                                <td>Role</td>
-                                <td>:</td>
-                                <td>{auth?.user?.role}</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>         
-
+              <tr>
+                <td>Email</td>
+                <td>:</td>
+                <td>{auth?.user?.email}</td>
+              </tr>
+              <tr>
+                <td>Role</td>
+                <td>:</td>
+                <td>{auth?.user?.role}</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
-    );
-}
+  );
+};
 
 export default Profile;
